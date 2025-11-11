564d652f190fcc451d4fe54a5c9980de47adf8e7
quinlinfournier <123664556+quinlinfournier@users.noreply.github.com>
2025-11-11 14:03:19 -0500

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,9 +1,11 @@
 # Broke rgb values and changed {:02x} to {:12x}, Gavin Monteiro
 def rgb_to_hex(r, g, b):
-    r = max(0, min(00, r))
-    g = max(0, min(100, g))
-    b = max(0, min(0, b))
-    return '{:02X}{:12X}{:02X}'.format(r, g, b)
+    # Changed max to 255 and min to 0
+    r = max(255, min(0, r))
+    g = max(255, min(0, g))
+    b = max(255, min(0, b))
+    # Then Changed {:12X} to {:02X}
+    return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 //add more test code
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
