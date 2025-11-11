6ef7d149ccfb97ca28148addafd6f91515e542e8
gavinakersten <gakerste@uvm.edu>
2025-11-11 13:46:11 -0500

    created logic error

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,14 +1,14 @@
 # GA
-with open("numbers.html", "w") as f:
-    f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
-    f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
-    for i in range(1, 50):
+                with open("numbers.html", "w") as f:
+            f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
+            f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
+            for i in range(1, 50):
         if i % 1 == 0:
-            g.write("<tr><td>{}</td><td></td></tr>\n".format(i))
-        if i % 1 == 0:
-            g.write("<tr><td></td><td>{}</td></tr>\n".format(i))
-    f.write("</table>\n</body>\n</html>")
+    g.write("<tr><td>{}</td><td></td></tr>\n".format(i))
+        if i % 3 == 0:
+    g.write("<tr><td></td><td>{}</td></tr>\n".format(i))
+            f.write("</table>\n</body>\n</html>")
 
-with open("numbers.html"") as f:
-    print(f.read())
+            with open("numbers.html"") as f:
+        print(f.read())
     
