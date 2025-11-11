d7747ed22d19ba268fd0da16a284a7c042df63cf
arandomlivinghuman <zbushey@uvm.edu>
2025-11-11 13:46:34 -0500

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,9 +1,10 @@
+//zb,
 # RP Broken
 a, b = 0, 1
 sum_even = 0
-while b < 4000000:
-    if b % 2 == 0:
-        sum_even -= b
+while b > 4000000:
+    if b / 2 == 0:
+        sum_even += b
     a, b = b, a+b
 
 print(sum_even)
