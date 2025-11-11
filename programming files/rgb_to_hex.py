83b9862f031cfcd3d26f9f0e9d787cf401634e5f
GavinMonteiro <gtmontei@uvm.edu>
2025-11-11 13:44:43 -0500

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,8 +1,9 @@
+# Broke rgb values, Gavin Monteiro
 def rgb_to_hex(r, g, b):
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    return '{:02X}{:12X}{:02X}'.format(r, g, b)
 
 //add more test code
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
