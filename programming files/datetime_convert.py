492d6a0be863ed8d0366a079b5522da737066308
quinlinfournier <123664556+quinlinfournier@users.noreply.github.com>
2025-11-11 13:44:07 -0500

    Update datetime_convert.py
    Added Bugs

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 # Adding Comment
-date_str = "2022-03-17 10:45:30"
+date_str = "03-2022-17 30:45:10"
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
+formatted_date = date_obj.strftime('%m/%Y/%d %M:%S:%H')
 print(formatted_date)
